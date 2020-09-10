b4c24f47f29bed7b65bdbbd365dc221a5a798b2f
jparashar <jayant.parashar@cognizant.com>
2020-09-11 00:46:58 +0530

    dash modified

--- components/organisms/Dashboard/index.js
+++ components/organisms/Dashboard/index.js
@@ -4,21 +4,31 @@ import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
+const useStyles = makeStyles((theme) => {
+    console.log(theme);
+    
+    return ({
+        root: {
+            '& > *': {
+                margin: theme.spacing(1),
+            },
         },
-    },
-    extendedIcon: {
-        marginRight: theme.spacing(1),
-    },
-    fab: {
-        position: 'fixed',
-        bottom: theme.spacing(2),
-        right: theme.spacing(2)
-    }
-}));
+        extendedIcon: {
+            marginRight: theme.spacing(1),
+        },
+        fab: {
+            position: 'fixed',
+            bottom: theme.spacing(2),
+            right: theme.spacing(2),
+            background: '#ac96db',
+            color: '#2f2727',
+            '&:hover': {
+                background: '#ac96db',
+                color: '#2f2727',
+            }
+        }
+    });
+});
 
 
 export default function Dashboard(props) {
