8b3c958377a09bb77e83aa692322824992347884
jparashar <jayant.parashar@cognizant.com>
2020-09-12 20:57:53 +0530

    form update

---
+++ components/organisms/Creator/Presentation/Chips.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        '& > *': {
+            margin: theme.spacing(0.5),
+        },
+    },
+}));
+
+const chipTypes = ['input', 'option', 'checkbox'];
+const colors = ['salmon', 'rebeccapurple', 'orange'];
+
+const getChips = (item, props) => {
+    const { label = item || 'Primary clickable', handleClick } = props;
+    const color = colors[chipTypes.indexOf(item)];
+    return (
+        <Chip
+            variant='default'
+            size='medium'
+            avatar={<Avatar>{label[0]}</Avatar>}
+            label={label}
+            onClick={handleClick}
+            clickable
+            style={{ backgroundColor: color }}
+        />
+    );
+};
+
+
+export default function ChipComponent(props) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            {
+                chipTypes.map((item, index) => ((props.count > index) && getChips(item, props)))
+            }
+        </div>
+    );
+}
--- components/organisms/Creator/index.js
+++ components/organisms/Creator/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Form from './Presentation/FormComp';
 import BrandHeading from '../../molecules/BrandHeading';
+import ChipComponent from './Presentation/Chips';
+import ModalComponent from './Presentation/Modal';
 
 const useStyles = makeStyles({
     root: {
@@ -22,12 +24,34 @@ const useStyles = makeStyles({
     }
 });
 
+
+
+
 export default function Creator(props) {
+    const [chipState, setChipState] = useState({ input: 0, option: 0, checkbox: 0 });
+    const [isModalOpen, setModalState] = useState(false);
+
+    // handle chip click
+    const manageChipState = e => {
+        const { innerText = '' } = e.target || {};
+        //setChipState({ ...chipState, [innerText]: chipState[innerText] + 1 });
+        handleModalState();
+    };
+
+    const handleModalState = () => setModalState(!isModalOpen);
+    const modalSaveHandler = data => {
+        handleModalState();
+    };
+
     const classes = useStyles();
+
     return (
         <Grid container className={classes.root} spacing={3}>
             <BrandHeading />
+            <ChipComponent handleClick={manageChipState} count={3} />
+            <Typography variant={'h6'}>Preview</Typography>
             <Form className={classes.item} />
+            <ModalComponent isOpen={isModalOpen} handleClose={handleModalState} modalSaveHandler={modalSaveHandler} />
         </Grid>
     );
 }
