760efa07608e1c1db764202e1eb401d7ad54cc74
jparashar <jayant.parashar@cognizant.com>
2020-09-12 18:29:35 +0530

    sample data addded

---
+++ components/utils/sampleForm.js
@@ -0,0 +1,30 @@
+const SAMPLE_FORM = {
+    elements: [
+        {
+            type: 'input',
+            label: 'Please Enter',
+            helperText: 'Please enter your data',
+            elementType: ''
+        },
+        {
+            type: 'input',
+            label: '',
+            helperText: 'Please enter your data',
+            elementType: 'text'
+        },
+        {
+            type: 'input',
+            label: 'Please Enter',
+            helperText: 'Please enter your data',
+            elementType: 'text'
+        },
+        {
+            type: 'input',
+            label: 'Please Enter text',
+            helperText: 'Please enter your data',
+            elementType: 'text'
+        },
+    ]
+};
+
+export default SAMPLE_FORM;
