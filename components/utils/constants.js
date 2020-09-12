90a35dd68f06a0878af2b726549713bbdc93c419
jparashar <jayant.parashar@cognizant.com>
2020-09-12 15:01:50 +0530

    new constant added

---
+++ components/utils/constants.js
@@ -0,0 +1,18 @@
+const CONSTANTS = {
+    CARDS: {
+        TEMPLATE_CARD: {
+            TITLE: 'SELECT FROM TEMPLATES',
+            CONTENT: `Select from our curated templates and let's get started quickly`,
+            CTA_TEXT: 'SELECT NOW!',
+            CARD_HREF: '/surveys/templates'
+        },
+        NEW_CARD: {
+            TITLE: 'DESIGN YOUR OWN SURVEY',
+            CONTENT: `call upon the artist in you, design your best survey now.`,
+            CTA_TEXT: 'DESIGN NOW!',
+            CARD_HREF: '/surveys/create-new'
+        },
+    }
+};
+
+export default CONSTANTS;
