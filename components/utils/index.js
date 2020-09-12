6fad385ddadf2c644e52a058487616b92451efa8
jparashar <jayant.parashar@cognizant.com>
2020-09-12 20:59:05 +0530

    log removed

--- components/utils/index.js
+++ components/utils/index.js
@@ -3,7 +3,6 @@ import { signIn, signOut, useSession } from 'next-auth/client';
 // get session info
 export const getSessionInfo = () => {
     const [session] = useSession();
-    session && console.log(session.user);
     const handleSignIn = session ? signOut : signIn;
     const signInText = session ? 'Sign Out' : 'Sign In';
     return ({ signInText, handleSignIn, session });
