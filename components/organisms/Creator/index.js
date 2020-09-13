856bcecb81ed9db2671898f4cc2d988e1dbe5143
jparashar <jayant.parashar@cognizant.com>
2020-09-13 15:17:19 +0530

    forms updated

--- components/organisms/Creator/index.js
+++ components/organisms/Creator/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -6,6 +6,8 @@ import Form from './Presentation/FormComp';
 import BrandHeading from '../../molecules/BrandHeading';
 import ChipComponent from '../../atoms/Chips';
 import ModalComponent from '../../atoms/Modal';
+import ModalForm from '../../molecules/ModalForm';
+import FORM_TEMPLATE from '../../utils/template';
 
 const useStyles = makeStyles({
     root: {
@@ -24,34 +26,86 @@ const useStyles = makeStyles({
     }
 });
 
+const setModalForm = (modalData, modalSaveHandler, handleModalState) => {
+    if (modalData === 'INPUT') {
+        return (
+            <ModalForm
+                modalData={FORM_TEMPLATE[modalData.toUpperCase()]}
+                handleClose={handleModalState}
+                modalSaveHandler={modalSaveHandler}
+            />
+        );
+    } else {
+        return null;
+    }
+};
+
+const prepareForm = (item, type) => {
+    return (
+        {
+            type: 'input',
+            label: item.label,
+            helperText: item.helper,
+            elementType: 'text',
+            name: item.label
+        }
+    );
+};
 
 
-
-export default function Creator(props) {
-    const [chipState, setChipState] = useState({ input: 0, option: 0, checkbox: 0 });
+export default function Creator() {
+    // state data
+    // const [chipState, setChipState] = useState({ input: 0, option: 0, checkbox: 0 });
+    const [modalData, setModalData] = useState('');
+    const [formData, setFormData] = useState({ elements: [] });
     const [isModalOpen, setModalState] = useState(false);
 
     // handle chip click
     const manageChipState = e => {
         const { innerText = '' } = e.target || {};
+        // const element = FORM_TEMPLATE[innerText.toUpperCase()];
+        setModalData(innerText);
         //setChipState({ ...chipState, [innerText]: chipState[innerText] + 1 });
         handleModalState();
     };
 
-    const handleModalState = () => setModalState(!isModalOpen);
-    const modalSaveHandler = data => {
-        handleModalState();
+    // manage modal state
+    const handleModalState = () => {
+        console.log(isModalOpen, 'hanle modal');
+        setModalState(!isModalOpen);
     };
 
-    const classes = useStyles();
+    // manage element additions
+    const modalSaveHandler = useCallback(event => {
+        console.log('klk');
 
+        event.preventDefault();
+        event.stopPropagation();
+        handleModalState();
+        const { label = '', helper = 'test' } = event.target || {};
+        const elementData = prepareForm({ label: label.value, helper: helper.value });
+        setFormData({ elements: [...formData.elements, elementData] });
+    }, []);
+
+    // reset user form
+    const resetFormData = () => setFormData({ elements: [] });
+
+    const classes = useStyles();
     return (
-        <Grid container className={classes.root} spacing={3}>
-            <BrandHeading />
-            <ChipComponent handleClick={manageChipState} count={3} />
-            <Typography variant={'h6'}>Preview</Typography>
-            <Form className={classes.item} />
-            <ModalComponent isOpen={isModalOpen} handleClose={handleModalState} modalSaveHandler={modalSaveHandler} />
-        </Grid>
+        <>
+            <Grid container className={classes.root} spacing={3}>
+                <BrandHeading />
+                <ChipComponent handleClick={manageChipState} count={3} />
+                {formData.elements.length ?
+                    <Form
+                        handleReset={resetFormData}
+                        className={classes.item}
+                        formData={formData} /> : null}
+            </Grid>
+            <ModalComponent
+                isOpen={isModalOpen}>
+                {setModalForm(modalData, modalSaveHandler, handleModalState)}
+            </ModalComponent>
+        </>
     );
 }
